d4a73ea75c6c2e65605b144d81dbb712e2b74f24
Tobias Christensen <tobiaschristensen@gmail.com>
2014-12-08 20:44:07 -0800

    added first rest api

---
+++ api/controllers/DemoObjController.js
@@ -0,0 +1,11 @@
+/**
+ * DemoObjController
+ *
+ * @description :: Server-side logic for managing demoobjs
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
